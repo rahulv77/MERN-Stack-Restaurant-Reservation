c943fa735aaba0a33a176a8f8021823b752b7705
rahulv77 <rahulvernekar777@gmail.com>
2024-07-07 15:27:47 +0530

    changes made

--- backend/app.js
+++ backend/app.js
@@ -19,6 +19,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/api/v1/reservation', ReservationRoute)
 
+app.get("/", (req, res, next)=>{return res.status(200).json({
+    success: true,
+    message: "HELLO WORLD AGAIN"
+  })})
+
 dbConnection();
 
 app.use(errorMiddleware)
